6cdb40c785c33a84fbb8a571691f05a80d23b7c9
Sravanib1911 <74718848+Sravanib1911@users.noreply.github.com>
2026-02-02 18:02:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import streamlit as st
+
+URL = "https://docs.google.com/spreadsheets/d/1l6E_b0RWIk_W4d7paNr9-vSX4SEzuC75FCBAuqxhZbE/export?format=xlsx"
+
+st.title("Budget Analyzer")
+
+# load directly from google sheets
+xls = pd.ExcelFile(URL)
+
+# -------- FIXED LIST --------
+fixed_df = pd.read_excel(xls, sheet_name="Fixed List")
+fixed_df.columns = ["Category", "Amount"]
+fixed_visible = fixed_df[fixed_df["Amount"].notna()]
+
+st.subheader("Fixed Budget")
+st.dataframe(fixed_visible)
+st.metric("Total Fixed Budget", fixed_visible["Amount"].sum())
+
+# -------- MONTH DATA --------
+df = pd.read_excel(xls, sheet_name="Jan 2026")
+
+df["Category"] = df["Categories-Fixed"].fillna(df["Categories-Random"])
+df = df[df["Category"].notna() & df["Debit"].notna()]
+
+st.subheader("Overall Spend")
+st.bar_chart(df.groupby("Category")["Debit"].sum())
+
+selected_cat = st.selectbox("Select Category", sorted(df["Category"].unique()))
+
+cat_df = df[df["Category"] == selected_cat]
+
+st.metric("Total Spent", cat_df["Debit"].sum())
+st.dataframe(cat_df[["Date","Debit","Reason"]])
+
+st.subheader("Reason Spend")
+st.bar_chart(cat_df.groupby("Reason")["Debit"].sum())
